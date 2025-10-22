86be0814c893903801d635d2dc468f7bbe248dd9
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-10-22 18:25:53 -0500

    aea

---
+++ scripts/login.js
@@ -0,0 +1,71 @@
+const pinDisplay = document.getElementById("pin-display");
+const keys = document.querySelectorAll(".key");
+let pin = "";
+
+// Actualiza el display con círculos
+function updateDisplay() {
+  if (pin.length === 0) {
+    pinDisplay.textContent = "----";
+  } else {
+    pinDisplay.textContent = "●".repeat(pin.length);
+  }
+}
+
+// Eventos de teclas
+keys.forEach(key => {
+  key.addEventListener("click", () => {
+    const value = key.textContent;
+
+    if (key.id === "clear") {
+      pin = pin.slice(0, -1);
+    } else if (key.id === "enter") {
+      validarPIN(pin);
+    } else {
+      if (pin.length < 6) { // máximo 6 dígitos
+        pin += value;
+      }
+    }
+    updateDisplay();
+  });
+});
+
+// Funcion de validacion
+function validarPIN(PIN){
+    try {
+        fetch('https://laimserver.duckdns.org/api/ValidarPIN/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include",
+            body: JSON.stringify({ pin: PIN })
+        }).then(res =>{
+            console.log("Respuesta status:", res.status);
+            console.log("Respuesta headers:", [...res.headers.entries()]);
+            return res.text();
+        })
+        .then(text=>{
+            console.log("Respuesta texto:", text);
+            let data;
+            try{
+                data = JSON.parse(text);
+            }catch{
+                console.error("No se pudo parsear JSON");
+                return;
+            }
+            console.log("Respuesta del servidor:", data);
+            if(data.success){
+                alert("ingreso correcto");
+                window.location.href = "../views/admin.html";
+            } else {
+                alert("ingreso incorreto");
+                pin = "";
+                updateDisplay();
+            }
+        }).catch(error=> console.error("Error en :", error));
+
+    
+    }catch (error) {
+        console.error("Error al validar el PIN:", error);
+    }
+}
